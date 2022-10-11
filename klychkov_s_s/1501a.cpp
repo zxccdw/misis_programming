2c50a835518d8d88504ff191eee6e4885e78de3a
zxccdw <57716489+zxccdw@users.noreply.github.com>
2022-10-11 11:02:10 +0400

    Add files via upload

---
+++ klychkov_s_s/1501a.cpp
@@ -0,0 +1,102 @@
+#if 0
+#pragma GCC optimize("O3,unroll-loops,fast-math")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#endif
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <map>
+#include <chrono>
+#include <set>
+#include <stack>
+#include <vector>
+#include <time.h>
+#include <random>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <bitset>
+#include <complex>
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define min(a, b) ((a < b) ? a : b)
+#define max(a, b) ((a < b) ? b : a)
+#define ALL(a) a.begin(), a.end()
+#define sp << " "
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+inline void smax(int& a, int b) { if (b > a)a = b; }
+inline void smin(int& a, int b) { if (b < a)a = b; }
+inline void smax(ll& a, ll b) { if (b > a)a = b; }
+inline void smin(ll& a, ll b) { if (b < a)a = b; }
+
+
+//#define ll __int128
+//#define int ll
+//#define char ll
+//#define double ll
+
+//using namespace std;
+
+const std::string DIGITS = "0123456789";
+const std::string ALPH = "abcdefghijklmnopqrstuvwxyz";
+std::mt19937 rng((unsigned int)(time(nullptr) + 'm' + 'a' + 'n' + 'u' + 'l'));
+std::uniform_int_distribution<int>uid(-1e9, 1e9);
+const double PI = 2 * acos(0.0);
+constexpr ll LLINF = ll(2e18) + 13;
+constexpr int INF = int(1e9) + 7;
+constexpr int SIZE = 1e5 + 10;
+
+
+void solve() {
+	int n, a_i, b_i;
+	int vr;
+	std::vector<std::pii> ar;
+	std::vector<int> tm, t_obr;
+	std::cin >> n;
+	for (int i = 0; i < n; ++i) {
+		std::cin >> a_i >> b_i;
+		ar.push_back(std::mp(a_i, b_i));
+
+	}
+	t_obr.push_back(ar[0].first);
+	for (int i = 1; i < n; ++i) {
+		t_obr.push_back(ar[i].first - ar[i - 1].second);
+	}
+	for (int i = 0; i < n; ++i) {
+		std::cin >> a_i;
+		tm.push_back(a_i);
+	}
+	int t = 0;
+	for (int i = 0; i < n; ++i) {
+		t += tm[i] + t_obr[i];
+		if (i == n - 1) {
+			break;
+		}
+		t = max(t + ceil((ar[i].second - ar[i].first) / 2.0), ar[i].second);
+	}
+	std::cout << t << std::endl;
+}
+
+
+signed main() {
+	std::ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+#ifdef _DEBUG
+	freopen("C:\\Users\\zxccdw\\source\\repos\\CMakeProject1\\input.txt", "r", stdin);
+	freopen("C:\\Users\\zxccdw\\source\\repos\\CMakeProject1\\output.txt", "w", stdout);
+#endif
+	int t;
+	std::cin >> t;
+	while (t--) {
+		solve();
+	}
+}
